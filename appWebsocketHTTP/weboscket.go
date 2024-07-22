63cf6ae5511d8a181faf6eba930e008f3d0ef4aa
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-22 11:30:19 +0200

    refactoring

--- appWebsocketHTTP/weboscket.go
+++ appWebsocketHTTP/weboscket.go
@@ -12,6 +12,29 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+func (app *AppWebsocketHTTP) GetWebsocketComponentConfig() *Config.Websocket {
+	return &Config.Websocket{
+		Pattern: "/ws",
+		Http: &Config.Http{
+			Server: &Config.TcpServer{
+				Port: 8443,
+			},
+			Blacklist: []string{},
+			Whitelist: []string{},
+		},
+		HandleClientMessagesSequentially: false,
+		ClientMessageCooldownMs:          0,
+		ClientWatchdogTimeoutMs:          20000,
+		Upgrader: &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	}
+}
+
 func (app *AppWebsocketHTTP) GetWebsocketMessageHandlers() map[string]Node.WebsocketMessageHandler {
 	return map[string]Node.WebsocketMessageHandler{
 		"startGame": func(node *Node.Node, websocketClient *Node.WebsocketClient, message *Message.Message) error {
@@ -111,24 +134,5 @@ func (app *AppWebsocketHTTP) OnDisconnectHandler(node *Node.Node, websocketClien
 		if errorLogger := node.GetErrorLogger(); errorLogger != nil {
 			errorLogger.Log(Error.New("Error sending end message for game: "+gameId, err).Error())
 		}
-	}
-}
-
-func (app *AppWebsocketHTTP) GetWebsocketComponentConfig() *Config.Websocket {
-	return &Config.Websocket{
-		Pattern: "/ws",
-		Server: &Config.TcpServer{
-			Port: 8443,
-		},
-		HandleClientMessagesSequentially: false,
-		ClientMessageCooldownMs:          0,
-		ClientWatchdogTimeoutMs:          20000,
-		Upgrader: &websocket.Upgrader{
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-		},
 	}
 }
