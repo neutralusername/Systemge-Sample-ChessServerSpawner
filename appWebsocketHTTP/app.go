31c336877b4945520addfb8e3cc8ba68a27bd282
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-22 10:11:47 +0200

    implemented dashboard

--- appWebsocketHTTP/app.go
+++ appWebsocketHTTP/app.go
@@ -19,9 +19,9 @@ func New() *AppWebsocketHTTP {
 
 func (app *AppWebsocketHTTP) GetCommandHandlers() map[string]Node.CommandHandler {
 	return map[string]Node.CommandHandler{
-		"move": func(node *Node.Node, args []string) error {
+		"move": func(node *Node.Node, args []string) (string, error) {
 			if len(args) != 6 {
-				return Error.New("Invalid move command", nil)
+				return "", Error.New("Invalid move command", nil)
 			}
 			gameId := args[0]
 			playerId := args[1]
@@ -31,9 +31,9 @@ func (app *AppWebsocketHTTP) GetCommandHandlers() map[string]Node.CommandHandler
 			colTo := args[5]
 			err := app.handleMove(node, gameId, playerId, rowFrom+" "+colFrom+" "+rowTo+" "+colTo)
 			if err != nil {
-				return Error.New("Error handling move", err)
+				return "", Error.New("Error handling move", err)
 			}
-			return nil
+			return "success", nil
 		},
 	}
 }
